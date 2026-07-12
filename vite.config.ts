1fcfc0fdce59411bbc2e4d0b679fe7ddb2ea9aba
Super Yoshi 10000 <SuperYoshi10000@gmail.com>
2026-07-12 15:17:05 -0400

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd());
 	return {
+		plugins: [sveltekit()],
 		server: {
 			allowedHosts: ["localhost", "hackatimechart.mitchk.hackclub.app"]
 		}
